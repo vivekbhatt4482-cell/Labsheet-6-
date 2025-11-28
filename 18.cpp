cd99f49ac5424156cad223377fdf010250993c9f
vivekbhatt4482-cell <vivekbhatt4482@gmail.com>
2025-11-28 12:12:25 +0530

    Binary Search for Integers

---
+++ 18.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cin >> key;
+
+    int low = 0, high = n - 1;
+
+    while(low <= high) {
+        int mid = (low + high) / 2;
+
+        if(arr[mid] == key) {
+            cout << "Found at index " << mid;
+            return 0;
+        }
+        else if(arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    cout << "Not found";
+    return 0;
+}
